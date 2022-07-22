0bbd64fe2f33a1ef76de976e19f38ed7b5a563c0
27ugeen <27ugeen@gmail.com>
2022-07-22 11:55:50 +0300

    fixed class User

--- Navigation/User/User.swift
+++ Navigation/User/User.swift
@@ -8,19 +8,22 @@
 import Foundation
 
 final class User: Equatable {
-    static func == (lhs: User, rhs: User) -> Bool {
-        true
-    }
-    
+    //MARK: - props
     let id: String
     let email: String
     let password: String
     
+    //MARK: - init
     init(id: String = UUID().uuidString, email: String, password: String) {
         self.id = id
         self.email = email
         self.password = password
     }
+    
+    //MARK: - methods
+    static func == (lhs: User, rhs: User) -> Bool {
+        return lhs.id == rhs.id &&
+        lhs.email == rhs.email &&
+        lhs.password == rhs.password
+    }
 }
-
-
